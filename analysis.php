288839844938391497bf4f037d60d99f470027f7
pipiscrew <pipiscrew@gmail.com>
2017-02-22 19:41:51 +0100

    sql, to replace linq added in comments

--- analysis.php
+++ analysis.php
@@ -136,6 +136,18 @@ foreach($progress_list as $row) {
 
 
 ////////////////////////////////////////////////////////////////////// ------------------- LINQ [START]
+/*
+//doing by SQL
+$q = "select COUNTRY, COUNT(COUNTRY) as counter from jobs where (CLOSE_TIME between '{$dtp_start}'
+ and '{$dtp_end}') ".(empty($user_ids) ? "" : $user_ids).(empty($countries) ? "" : $countries).' group by COUNTRY order by counter desc';
+$rows = $mysql->getSet($q, null);
+
+$countries_all = array();
+$countries_all[] = array('Task', 'Hours per Day');
+foreach($rows as $row) {
+	$countries_all[] = array($row['COUNTRY'], (int) $row['counter']);
+}
+*/
 
 //construct SQL
 $q = "select * from jobs where (DATE_CREATED between '{$dtp_start}'
